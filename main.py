b21fe2341ff63d5dc4fb0ebb574a965168262aff
TheMojoMan <themojoman@gmx.net>
2026-01-02 16:30:10 +0100

    Removed: old print statements, added: check if input file exists

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from stats import get_num_words, get_num_chars, get_sorted_list_of_dicts
 
-import sys
+import sys, os
 
-def get_book_text(filepath):
-    with open(filepath) as f:
-        return f.read()
-
+def get_book_text(file_path):
+    if os.path.exists(file_path):
+        with open(file_path) as f:
+            return f.read()
+    else:
+       return "-1"
+       
 def print_report(filepath,num_words,sorted_list):
     out = f"""============ BOOKBOT ============
 Analyzing book found at {filepath}...
@@ -22,14 +25,16 @@ def main():
     if len(sys.argv) != 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
+        
     filepath = sys.argv[1]
     text = get_book_text(filepath)
+    if text == "-1":
+        print("File error: file does not exist")
+        sys.exit(1)
+        
     num_words = get_num_words(text)
     dict_num_chars = get_num_chars(text)
     sorted_list = get_sorted_list_of_dicts(dict_num_chars)
-
-    #print(f"Found {num_words} total words")
-    #print(dict_num_chars)
-    #print(get_sorted_list_of_dicts(dict_num_chars))
     print_report(filepath, num_words, sorted_list)
+
 main()
