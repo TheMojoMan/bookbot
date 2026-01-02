c8c5053f93a506d27a6dcb1ca523792ecbdc02cb
TheMojoMan <themojoman@gmx.net>
2026-01-02 16:22:25 +0100

    Removed: Hard coded file path, added: use command line argument for the book to be analyzed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from stats import get_num_words, get_num_chars, get_sorted_list_of_dicts
 
+import sys
+
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
@@ -17,7 +19,10 @@ Found {num_words} total words
     print("============= END ===============")
 
 def main():
-    filepath = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath = sys.argv[1]
     text = get_book_text(filepath)
     num_words = get_num_words(text)
     dict_num_chars = get_num_chars(text)
